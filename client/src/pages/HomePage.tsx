b23128fb727fa07f55fdd3cf3a0008ea87ce4c6e
jaystas <jays.staigers@gmail.com>
2026-03-20 08:02:09 -0500

    position

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -3,9 +3,12 @@ import { ChatEditor } from "@/components/editor/ChatEditor"
 export function HomePage() {
   return (
     <div className="page-canvas home-page">
-      <div className="home-page__content">
-        <div className="home-page__editor-offset">
-          <ChatEditor />
+      <div className="home-page__columns">
+        <div className="home-page__column home-page__column--left" />
+        <div className="home-page__column home-page__column--right">
+          <div className="home-page__editor-shell">
+            <ChatEditor />
+          </div>
         </div>
       </div>
     </div>
